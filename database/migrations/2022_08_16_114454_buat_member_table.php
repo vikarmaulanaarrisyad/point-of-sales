0ec62135dca6f31f58cd17a2f493c10c9f3f40f7
vikarmaulanaarrisyad <vikar.maulana.arrisyad@gamil.com>
2022-08-16 19:31:07 +0700

    first commit

---
+++ database/migrations/2022_08_16_114454_buat_member_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('member', function (Blueprint $table) {
+            $table->increments('id_member');
+            $table->string('kode_member')->unique();
+            $table->string('nama');
+            $table->text('alamat')->nullable();
+            $table->string('telepon')->nullable();
+            $table->timestamps();
+        });
+        
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('member');
+    }
+};
---
+++ resources/views/partials/sidebar.blade.php
@@ -0,0 +1,107 @@
+<aside class="main-sidebar">
+    <section class="sidebar">
+        <div class="user-panel">
+            <div class="pull-left image">
+                <img src="{{ asset('AdminLTE-2/dist/img/user2-160x160.jpg') }}" class="img-circle" alt="User Image">
+            </div>
+            <div class="pull-left info">
+                <p>Alexander Pierce</p>
+                <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
+            </div>
+        </div>
+
+        <ul class="sidebar-menu" data-widget="tree">
+            <li>
+                <a href="#">
+                    <i class="fa fa-dashboard"></i> <span>Dashboard</span>
+                </a>
+            </li>
+
+            <li class="header">MASTER</li>
+            <li>
+                <a href="#">
+                    <i class="fa fa-cube"></i> <span>Kategori</span>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <i class="fa fa-cubes"></i> <span>Produk</span>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <i class="fa fa-id-card"></i> <span>Member</span>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <i class="fa fa-truck"></i> <span>Supplier</span>
+                </a>
+            </li>
+
+            <li class="header">TRANSAKSI</li>
+            <li>
+                <a href="#">
+                    <i class="fa fa-money"></i> <span>Pengeluaran</span>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <i class="fa fa-download"></i> <span>Pembelian</span>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <i class="fa fa-upload"></i> <span>Penjualan</span>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <i class="fa fa-cart-arrow-down"></i> <span>Transaksi Lama</span>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <i class="fa fa-cart-arrow-down"></i> <span>Transaksi Baru</span>
+                </a>
+            </li>
+
+            <li class="header">REPORT</li>
+            <li>
+                <a href="#">
+                    <i class="fa fa-file-pdf-o"></i> <span>Laporan</span>
+                </a>
+            </li>
+
+            <li class="header">SYSTEM</li>
+            <li>
+                <a href="#">
+                    <i class="fa fa-users"></i> <span>User</span>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <i class="fa fa-cog"></i> <span>Pengaturan</span>
+                </a>
+            </li>
+
+            {{--  <li class="treeview">
+                <a href="#">
+                    <i class="fa fa-files-o"></i>
+                    <span>Layout Options</span>
+                    <span class="pull-right-container">
+                        <span class="label label-primary pull-right">4</span>
+                    </span>
+                </a>
+                <ul class="treeview-menu">
+                    <li><a href="pages/layout/top-nav.html"><i class="fa fa-circle-o"></i> Top Navigation</a>
+                    </li>
+                    <li><a href="pages/layout/boxed.html"><i class="fa fa-circle-o"></i> Boxed</a></li>
+                    <li><a href="pages/layout/fixed.html"><i class="fa fa-circle-o"></i> Fixed</a></li>
+                    <li><a href="pages/layout/collapsed-sidebar.html"><i class="fa fa-circle-o"></i> Collapsed
+                            Sidebar</a></li>
+                </ul>
+            </li>  --}}
+        </ul>
+    </section>
+</aside>
